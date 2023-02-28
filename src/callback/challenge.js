892e74f1bc067b28ff4e2552538f91d6188cbdad
AndrewArian <abradan97@gmail.com>
2023-02-28 20:54:24 -0300

    Completed the class 13, learning the use of fetch.

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,35 +1,33 @@
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest; 
+import fetch from 'node-fetch';
 const API = 'https://api.escuelajs.co/api/v1';
+function fetchData(urlAPI) {
+    return fetch(urlAPI);
+};
 
-function fetchData(urlApi, callback) {
-    let xhttp = new XMLHttpRequest();
+/*
+fetchData(`${API}/products`)
+ .then(response => response.json())
+ .then(products => {
+    console.log(products);
+ })
+ .then(() => {
+    console.log("hola")
+ })*/
 
-    xhttp.open('GET', urlApi, true);
-    xhttp.onreadystatechange = function (event) {
-        if (xhttp.readyState === 4){
-            if(xhttp.status === 200){
-                callback(null, JSON.parse(xhttp.responseText));
-            } else{
-                const error = new Error('Error' + urlApi);
-                return callback(error, null);
-            }
-        }
-    }
-    xhttp.send(); 
-}
-
-
-
-
-fetchData(`${API}/products`, function(error1, data1){
-    if (error1) return console.error(error1);
-    fetchData(`${API}/products/${data1[0].id}`, function(error2, data2){
-        if (error2) return console.error(error2);
-        fetchData(`${API}/categories/${data2?.category?.id}`, function(error3, data3){
-            if (error3) return console.error(error3);
-            console.log(data1[0]);
-            console.log(data2.title);
-            console.log(data3.name);
-        })
-    })
-})
+ fetchData(`${API}/products`)
+ .then(response => response.json())
+ .then(products => {
+   console.log(products)
+    return fetchData(`${API}/products/${products[0].id}`)
+ })
+ .then(response => response.json())
+ .then(product => {
+   console.log(product.title);
+    return fetchData(`${API}/categories/${product.category.id}`)
+ })
+ .then(response => response.json())
+ .then(category => { 
+   console.log(category.name);
+ })
+ .catch(err => console.log(err))
+ .finally(() => console.log('Finally'));
--- src/callback/index.js
+++ src/callback/index.js
@@ -2,7 +2,7 @@ const promise = new Promise(function (resolve, reject) {
     resolve('Todo nice')
 });
 
-const cows = 9;
+const cows = 11;
 
 const countCows = new Promise(function (resolve, reject){
     if(cows > 10) {
@@ -14,4 +14,6 @@ const countCows = new Promise(function (resolve, reject){
 
 countCows.then((result) => {
     console.log(result);
-})
+}).catch((error) => {
+    console.log(error);
+}).finally(() => console.log('Finally'))
